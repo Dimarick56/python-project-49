77c1793940a2195680a3759f5d7c6a4b9f95fa07
Dimarick <diman9556@mail.ru>
2024-04-23 20:12:11 +0500

    s

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+from brain_games import cli
+
 def main():
 
     print('Welcome to the Brain Games!')
+
+    cli.welcome_user()
 
 if __name__ == '__main__':
 
     main()
 
-from brain_games import cli
 
-cli.welcome_user()
+
+
 
 
